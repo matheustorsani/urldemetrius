ecdabeaa5f40e88498a197eeaf89a5d9aa1a239c
matheustorsani <zkingdragoon@gmail.com>
2021-05-20 11:17:53 -0300

    suporte pra celular :)

---
+++ src/script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById('brunaoamigao').innerHTML = 'Esse site é mais otimizado se o usuario estiver utilizando um computador.';
+        const brunao = document.getElementById(`brunaoamigao`)
+        const demetrius = document.getElementById('demetrius')
+        const gamingsetup = document.getElementById('gamingsetup')
+        const content = document.getElementById('content')
+        const social = document.getElementById('social')
+        const redes = document.getElementById('redes')
+        demetrius.style.paddingLeft = '48px';
+        gamingsetup.style.paddingLeft = '48px';
+        content.style.paddingLeft = '50px';
+        social.style.paddingLeft = '50px';
+        redes.style.paddingLeft = '50px';
+        brunao.style.paddingLeft = '40px';
+    }
+}
+
+
