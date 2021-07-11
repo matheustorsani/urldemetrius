fe923cf94553b3fa288fd19bd5ab34c57c9745f1
matheustorsani <zkingdragoon@gmail.com>
2021-07-11 18:01:21 -0300

    rework

--- src/script.js
+++ src/script.js
@@ -1,19 +1,9 @@
 window.onload = function() {
+    const lang = navigator.language
+    const information = document.getElementById('information')
+    if (lang === "pt-BR") return information.innerHTML = 'Meu nome é Matheus Torsani, na internet conhecido como "demetrius". Sou um estudante focado em ser um Programador Full Stack :)'
+    information.innerHTML = 'My name is Matheus Torsani, known on the internet as "demetrius". Im a student focused on being a Full Stack Programmer :)'
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        document.getElementById('brunaoamigao').innerHTML = 'Esse site é mais otimizado se o usuario estiver utilizando um computador.';
-        const brunao = document.getElementById(`brunaoamigao`)
-        const demetrius = document.getElementById('demetrius')
-        const gamingsetup = document.getElementById('gamingsetup')
-        const content = document.getElementById('content')
-        const social = document.getElementById('social')
-        const redes = document.getElementById('redes')
-        demetrius.style.paddingLeft = '48px';
-        gamingsetup.style.paddingLeft = '48px';
-        content.style.paddingLeft = '50px';
-        social.style.paddingLeft = '0';
-        redes.style.paddingLeft = '17px';
-        brunao.style.paddingLeft = '40px';
+        // coming soon, 
     }
 }
-
-
