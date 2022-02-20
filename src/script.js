c66736689ef9181b4780c24485ece9a1e305d423
matheustorsani <zkingdragoon@gmail.com>
2022-02-20 18:19:31 -0300

    mais um

--- src/script.js
+++ src/script.js
@@ -2,8 +2,4 @@ window.onload = function() {
     const lang = navigator.language
     const information = document.getElementById('information')
     if (lang === "pt-BR") return information.innerHTML = 'Meu nome é Matheus Torsani, na internet conhecido como "demetrius". Sou um estudante focado em ser um Programador Full Stack :)'
-    information.innerHTML = 'My name is Matheus Torsani, known on the internet as "demetrius". Im a student focused on being a Full Stack Programmer :)'
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        // coming soon, 
-    }
 }
