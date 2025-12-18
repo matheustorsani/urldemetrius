8efb097429aebb8311fe3aea110968046cd034f2
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 <zkingdragoon@gmail.com>
2025-12-17 22:29:09 -0300

    Resolvendo deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Main from './../views/Main.vue'
-import Forum from './../views/Forum.vue'
-import Projects from './../views/Projects.vue'
-import TheEndOfTheWorld from './../views/TheEndOfTheWorld.vue'
-import NotFound from './../views/NotFound.vue'
+// import Main from './../views/Main.vue'
+// import Forum from './../views/Forum.vue'
+// import Projects from './../views/Projects.vue'
+// import TheEndOfTheWorld from './../views/TheEndOfTheWorld.vue'
+// import NotFound from './../views/NotFound.vue'
 
 const routes = [
-  { path: '/', component: Main },
-  { path: '/forum', component: Forum },
-  { path: '/projects', component: Projects },
-  { path: '/newyear', component: TheEndOfTheWorld },
+  { path: '/', component: () => import('./../views/Main.vue') },
+  { path: '/forum', component: () => import('./../views/Forum.vue') },
+  { path: '/projects', component: () => import('./../views/Projects.vue') },
+  { path: '/newyear', component: () => import('./../views/TheEndOfTheWorld.vue') },
 
-  { path: '/:pathMatch(.*)*', component: NotFound }
+  { path: '/:pathMatch(.*)*', component: () => import('./../views/NotFound.vue') },
 ]
 
 export const router = createRouter({
